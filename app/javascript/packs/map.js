7069082d01739e686b853f8f0106481c47a931c9
Pascal Laliberté <pascal@pascallaliberte.me>
2020-05-13 20:58:15 -0400

    stub map route, styles, pack

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('[data-behavior="map"]')
+
+  if (container) {
+    container.innerHTML = "<h1><code>insert map here</code></h1>"
+  }
+})
+
