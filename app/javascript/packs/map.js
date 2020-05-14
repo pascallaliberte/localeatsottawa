75f29f62f55f1c6ba2abdb46f783434caa98a0df
Pascal Laliberté <pascal@pascallaliberte.me>
2020-05-13 22:04:06 -0400

    Add full-screen map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,9 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('[data-behavior="map"]')
+  const el = document.querySelector('[data-behavior="map"]')
 
-  if (container) {
-    container.innerHTML = "<h1><code>insert map here</code></h1>"
+  if (!el) {
+    return
   }
+  
+  mapboxgl.accessToken = 'pk.eyJ1IjoicGFzY2FsbGFsaWJlcnRlIiwiYSI6ImNrYTYzenkwZTAycG0yd3MwbHNpZGYzZTUifQ.m5Op1SQuKoQrxKLrae7zsw';
+  
+  var map = new mapboxgl.Map({
+    container: el,
+    style: 'mapbox://styles/pascallaliberte/cka64b87u07fh1jnwa42b0brx',
+    center: [-75.687, 45.422],
+    zoom: 12
+  });
 })
 
